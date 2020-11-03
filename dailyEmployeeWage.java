667fd5808d38722b661295165aa6718aa8a377f8
nagalakshmi <nagalakshmiobillaneni1501@gmail.com>
2020-11-03 11:40:46 +0530

    Daily Employee wage

---
+++ dailyEmployeeWage.java
@@ -0,0 +1,12 @@
+public class dailyEmployeeWage
+{
+	public static void main(String args[])
+	{
+		int wage_per_hour=20;
+		int full_day_hour=8;
+		int salary;
+		salary=wage_per_hour*full_day_hour;
+		System.out.println("Daily Employee Wage is " +salary);
+	}
+}
+		
