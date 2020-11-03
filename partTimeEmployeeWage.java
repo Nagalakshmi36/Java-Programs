5e1ed5a60b4d19e075b71614a36a9874eddd2912
nagalakshmi <nagalakshmiobillaneni1501@gmail.com>
2020-11-03 12:44:29 +0530

    add part time employee

---
+++ partTimeEmployeeWage.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class partTimeEmployeeWage
+{
+	public static void main(String args[])
+	{
+		int salary;
+		int parttimehr=8,emprateperhr=20;
+		if(parttimehr==8)
+		{
+			salary=(parttimehr*emprateperhr);
+			System.out.println("employee wages is " +salary);
+		}
+	}
+}
+		
